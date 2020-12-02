677535994cf4945650cb221c776c612b57e6501e
Yakini <yakinilatha09@gmail.com>
2020-12-01 21:55:20 -0700

    fixed portfolio styles issue

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -9,23 +9,23 @@ class Portfolio extends Component {
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
           
-              <img alt={projects.title} src={projectImage} />
+              <img alt={projects.title} src={projectImage} style={{width: "300px", height: "200px"}} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
                  <h5>{projects.title}</h5>
                      <p>{projects.category}</p>
-                  </div>
+ 
                 </div>
-                <hr/>
-                <hr/>
-
-                <a href={projects.deployed} title={projects.title} target="_blank" >
-                <div className="link-icon" style={{marginLeft: "2em"}}><i className="fa fa-external-link"></i></div>
+                
+                <a href={projects.deployed} title={projects.title} target="_blank" rel="noopener noreferrer">
+                    <div className="link-icon" style= {{paddingLeft: "2em"}}><i className="fa fa-external-link"></i></div>
                 </a>
-                <a href={projects.github} title={projects.title}  target="_blank">
-                <div className="link-icon" style= {{paddingLeft: "2em"}}><i className="fa fa-github"></i></div>
+                <a href={projects.github} title={projects.title}  target="_blank" rel="noopener noreferrer">
+                    <div className="link-icon"><i className="fa fa-github"></i></div>
                 </a>
 
+                </div>
+  
             {/* <a href={projects.github} title={projects.title}  target="_blank">
 
               <img alt={projects.title} src={projectImage} />
