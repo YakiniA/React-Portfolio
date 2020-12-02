c916b80e7548b2ab1e26cddcf7dab2666c532832
Yakini <yakinilatha09@gmail.com>
2020-12-01 17:21:57 -0700

    fixed overlay issue

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -9,16 +9,37 @@ class Portfolio extends Component {
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
           
-               <img alt={projects.title} src={projectImage} />
+              <img alt={projects.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
                  <h5>{projects.title}</h5>
                      <p>{projects.category}</p>
                   </div>
                 </div>
-                <a href={projects.deployed} title={projects.title} className="link-icon mr-3"><i className="fa fa-external-link"></i></a>
-                <a href={projects.github} title={projects.title} className="link-icon"><i className="fa fa-github"></i></a>
+                <hr/>
+                <hr/>
 
+                <a href={projects.deployed} title={projects.title} target="_blank" >
+                <div className="link-icon" style={{marginLeft: "2em"}}><i className="fa fa-external-link"></i></div>
+                </a>
+                <a href={projects.github} title={projects.title}  target="_blank">
+                <div className="link-icon" style= {{paddingLeft: "2em"}}><i className="fa fa-github"></i></div>
+                </a>
+
+            {/* <a href={projects.github} title={projects.title}  target="_blank">
+
+              <img alt={projects.title} src={projectImage} />
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                      <p>{projects.category}</p>
+                  </div>
+                </div>
+
+              <div className="link-icon"><i className="fa fa-github"></i></div>
+            </a>  */}
+
+               
             {/* </a> */}
           </div>
         </div>
