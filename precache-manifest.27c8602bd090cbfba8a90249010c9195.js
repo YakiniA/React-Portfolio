e974609c35e55499310f9b4463a21e9309867ad1
Yakini <yakinilatha09@gmail.com>
2020-12-03 21:48:36 -0700

    Updates

---
+++ precache-manifest.27c8602bd090cbfba8a90249010c9195.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d96051c0ffb84ed52cdc6ba287f2249",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "9e82ba94f919e3843d75",
+    "url": "/React-Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4b661d602bcb76f3a952",
+    "url": "/React-Portfolio/static/js/2.c48e23ad.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/React-Portfolio/static/js/2.c48e23ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e82ba94f919e3843d75",
+    "url": "/React-Portfolio/static/js/main.df9eedef.chunk.js"
+  },
+  {
+    "revision": "89f5ed8e982227547873",
+    "url": "/React-Portfolio/static/js/runtime-main.3435375c.js"
+  },
+  {
+    "revision": "4bcd757ab1d7ff7dc109edc97e95ac7c",
+    "url": "/React-Portfolio/static/media/CSSImg.4bcd757a.png"
+  },
+  {
+    "revision": "bb721165997f3e3a3fcb050d0f9b58c1",
+    "url": "/React-Portfolio/static/media/HTMLImg.bb721165.png"
+  },
+  {
+    "revision": "03d471c6a43c17a3496228b5b43512e6",
+    "url": "/React-Portfolio/static/media/JSImg.03d471c6.png"
+  },
+  {
+    "revision": "a3af0655fc9a481401550b5be0e86cab",
+    "url": "/React-Portfolio/static/media/JqueryImg.a3af0655.png"
+  },
+  {
+    "revision": "ac0686a4662c1e4643d42c1946bb84f1",
+    "url": "/React-Portfolio/static/media/MERN.ac0686a4.png"
+  },
+  {
+    "revision": "97baad44ff7b48ac3ed7698f106edbf9",
+    "url": "/React-Portfolio/static/media/Yakini_Resume.97baad44.pdf"
+  },
+  {
+    "revision": "a5b14648713d4f1a1ef75646e53def80",
+    "url": "/React-Portfolio/static/media/javaImg.a5b14648.png"
+  },
+  {
+    "revision": "cb8f2e3e67d971e387aab34d5a43741a",
+    "url": "/React-Portfolio/static/media/jmeterPic.cb8f2e3e.png"
+  },
+  {
+    "revision": "56b7c596aed25950d1f263333bf0bf43",
+    "url": "/React-Portfolio/static/media/manual-testing.56b7c596.png"
+  },
+  {
+    "revision": "24670a166ce4fcad73c1e18c05068942",
+    "url": "/React-Portfolio/static/media/mongoDB.24670a16.png"
+  },
+  {
+    "revision": "43049b25ae0d1baf359aab52869fee28",
+    "url": "/React-Portfolio/static/media/mySQLImg.43049b25.png"
+  },
+  {
+    "revision": "270f55fffea36219b66a63fbe03ff497",
+    "url": "/React-Portfolio/static/media/nodeJS.270f55ff.png"
+  },
+  {
+    "revision": "559524d8f4492942185f522b347c69d7",
+    "url": "/React-Portfolio/static/media/react.559524d8.png"
+  },
+  {
+    "revision": "ce64f3a0f8b20d8f64513508ca5d6c59",
+    "url": "/React-Portfolio/static/media/seleniumImg.ce64f3a0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.da4856d10946cbc1d5a2d86230f95711.js"
+  "/React-Portfolio/precache-manifest.27c8602bd090cbfba8a90249010c9195.js"
 );
 
 self.addEventListener('message', (event) => {
